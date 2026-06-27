fb12c817ecca2b2ca64e993eed64443c1cbf6f0f
Alan Guerra <dev@reesearch64.tech>
2026-06-27 16:26:11 -0300

    feat: scaffold react app entry and global styles

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import { ThemeProvider } from './components/theme-provider.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+)
